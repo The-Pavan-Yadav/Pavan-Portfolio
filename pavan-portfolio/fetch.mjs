8717d93020a65eed1f3c05f585090cf414de13f8
PAVAN <jpavan2244@gmail.com>
2026-06-07 01:25:25 +0530

    Delete pavan-portfolio directory

--- pavan-portfolio/fetch.mjs
+++
@@ -1,6 +0,0 @@
-import https from "https";
-https.get("https://certain-neural-archive-labs.base44.app", (res) => {
-  let data = "";
-  res.on("data", (chunk) => data += chunk);
-  res.on("end", () => console.log(data));
-});
